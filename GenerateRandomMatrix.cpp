9fea548509499acc9b9a81a664bd4c1771b09d5f
RyogoTabuchi <ryogo.1008@gmail.com>
2019-06-06 11:55:20 +0900

    ランダムな配列を出力する部分を実装
    seedの決め方が一定なので、乱数が毎回同じ定数になっている。
    今後ここは修正したい.

--- GenerateRandomMatrix.cpp
+++ GenerateRandomMatrix.cpp
@@ -3,9 +3,12 @@
 
 using namespace std;
 
-template<class T> T generateRandomValue();
-template<> Q generateRandomValue(){
-  return Q("1/2");
+template<class T> T generateRandomValue(unsigned long seed);
+template<> Q generateRandomValue(unsigned long seed){
+  gmp_randclass r1(gmp_randinit_mt);
+  r1.seed(seed);
+  return Q(r1.get_z_bits(Z(10)));
+
 }
 
 template<class T> Mat<T> generateRandomMatrix(int n,int m){
@@ -13,7 +16,7 @@ template<class T> Mat<T> generateRandomMatrix(int n,int m){
   Mat<T> A(n,vector<T>(m,0));
   for(int i=0;i<n;i++){
     for(int j=0;j<m;j++){
-      A[i][j]=generateRandomValue<T>();
+      A[i][j]=generateRandomValue<T>(i*j+i+j);
     }
   }
   return A;
@@ -21,5 +24,5 @@ template<class T> Mat<T> generateRandomMatrix(int n,int m){
 
 int main(int argc,char *argv[]){
   int n=atoi(argv[1]),m=atoi(argv[2]);
-  printMatrix(generateRandomMatrix<Q>(n,m));
+  printMatrixInRow(generateRandomMatrix<Q>(n,m));
 }
