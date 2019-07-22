51fb8c7b9f0a2a3e4afe2f30cf1650432f037581
RyogoTabuchi <ryogo.1008@gmail.com>
2019-07-22 13:57:37 +0900

    fix inputs
    入力を標準入力から取得するように変更

--- MatrixRank.cpp
+++ MatrixRank.cpp
@@ -2,13 +2,24 @@
 #include"Matrix.h"
 
 int main(int argc,char* argv[]){
-  int I=atoi(argv[1]),J=atoi(argv[2]);
+  /*int I=atoi(argv[1]),J=atoi(argv[2]);
   Mat<Q> m(I,vector<Q>(J,0));
   for(int i=0;i<I;i++){
     for(int j=0;j<J;j++){
       m[i][j]=Q(argv[i*J+j+3]);
     }
+  }*/
+  int I,J;
+  cin>>I>>J;
+  Mat<Q> m(I,vector<Q>(J,0));
+  for(int i=0;i<I;i++){
+    for(int j=0;j<J;j++){
+      double x;
+      cin>>x;
+      m[i][j]=Q(x);
+    }
   }
+
   cout<<matrixRank(m)<<endl;
   printMatrix(m);
 }
